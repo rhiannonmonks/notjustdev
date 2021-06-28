40b261ff1958e96a3898e0e6d236f954a7d4c85d
Rhiannon Monks <rhiannon.monks@gmail.com>
2021-06-28 12:46:41 +0930

    styling home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,45 +11,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+  <main className={styles.main}>
+        {/* Hero */}
+  <div  className="flex flex-col md:flex-row p-5 max-w-screen-xl">
+         {/* Taglines */}
+    <div className="flex-1 mb-5">
+      <p className="text-xs text-secondary font-mono">Development | Code | Entrepreneurship</p>
+      
+      <h1 className="text-2xl my-3">I'm here to help you become a really good developer</h1>
+      <p className="text-sm text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et</p>
+    {/* button */ }
+    </div >
+    { /* Video */ }
+        <div className="flex-1 justify-center items-center mr-8">
+            <div className="flex-1 relative">
+              <Image
+                src="/images/video_img.png" // Route of the image file
+                height={144} // Desired size with correct aspect ratio
+                width={144} // Desired size with correct aspect ratio
+                alt="profile image"
+                layout="responsive"
+              />
+            <div className="absolute bottom-0 right-0 w-14 transform translate-y-9">
+              <Image
+                src="/images/brand_elements/pattern_4.svg" // Route of the image file
+                height={144} // Desired size with correct aspect ratio
+                width={144} // Desired size with correct aspect ratio
+                alt="profile image"
+                layout="responsive"
+              />
+            </div>
+          </div>
         </div>
+      </div>
       </main>
 
       <footer className={styles.footer}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ffe031',
+        secondary: '#8bd2bd'
+      },
+      textColor: {
+        primary: '#ffe031',
+        secondary: '#8bd2bd'
+      }
+    },
   },
   variants: {
     extend: {},
